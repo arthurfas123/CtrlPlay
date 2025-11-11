749cef456c83ce4744d1036e0a6e3709fb6e3d73
Arthur <Arthurfas123@gmail.com>
2025-11-11 12:01:35 -0300

    primeiro commit

---
+++ Aulas/script.js
@@ -0,0 +1,9 @@
+let numero1;
+let numero2;
+
+numero1 = prompt("Digite um valor: ");
+numero2 = prompt("Digite outro valor: ");
+
+let x = parseInt(numero1) + parseInt(numero2);
+
+console.log("X =  " + x);
